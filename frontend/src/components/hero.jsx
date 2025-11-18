57f3b6fee9e206cc5d38d244d0884ddcdefbfa9b
sachinv1604 <sachinvernekar29@gmail.com>
2025-11-18 17:30:26 +0530

    restart

--- frontend/src/components/hero.jsx
+++ frontend/src/components/hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <section className="hero" id="home"> <br></br> <br></br> <br></br><br></br>
       <div className="hero-content">
        
-        <h1 className="hero-title">Revolutionizing College Transportation</h1>
+        <h1 className="hero-title">Revolutionizing College bus Transportation</h1>
         <p className="hero-subtitle">
 
           Real-time tracking, smart notifications, and seamless coordination for students, coordinators, and drivers.
